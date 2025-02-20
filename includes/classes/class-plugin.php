e845e25eab9a97d1e29350f7029c96e20ea62780
nirajgirixd <nirajgiri9860@gmail.com>
2025-02-20 23:56:31 +0545

    Load json and provide method to retrieve json data

--- includes/classes/class-plugin.php
+++ includes/classes/class-plugin.php
@@ -20,11 +20,52 @@ class Plugin {
 	use Singleton;
 
 	/**
+	 * Stores plugin data from JSON file.
+	 * @var array
+	 */
+	private $json_data = array();
+
+	/**
 	 * Constructor method is used to initialize the fields.
 	 */
 	public function __construct() {
+
 		$this->load_classes();
 		$this->setup_hooks();
+		$this->load_json_data();
+	}
+
+	/**
+	 * Load plugin data from JSON file.
+	 *
+	 * @return void
+	 */
+	private function load_json_data() {
+
+		// Path to the JSON file containing table source data.
+		$json_path = WP_VIP_COMPATIBILITY_DIR . '/data/data.json';
+
+		if ( file_exists( $json_path ) ) {
+			// Read the JSON file contents.
+			$json_content = file_get_contents( $json_path );
+
+			// Decode the JSON data into an associative array.
+			$this->json_data = json_decode( $json_content, true ) ?: [];
+		}
+
+		// Check if decoding was successful.
+		if ( null === $this->json_data && json_last_error() !== JSON_ERROR_NONE) {
+			$this->json_data = array();
+		}
+	}
+
+	/**
+	 * Get the plugin data from JSON file.
+	 *
+	 * @return array
+	 */
+	public function get_json_data() {
+		return $this->json_data;
 	}
 	
 	/**
@@ -33,6 +74,7 @@ class Plugin {
 	 * @return void
 	 */
 	private function load_classes() {
+	
 		Assets::get_instance();
 		Ajax::get_instance();
 		Settings::get_instance();
@@ -44,7 +86,7 @@ class Plugin {
 	 * @return void
 	 */
 	public function setup_hooks() {
-		// Actions.
+
 		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
 	}
 
@@ -54,6 +96,7 @@ class Plugin {
 	 * @return void
 	 */
 	public function load_textdomain() {
+
 		load_plugin_textdomain( 'wp-vip-compatibility', false, WP_VIP_COMPATIBILITY_DIR . '/languages' );
 	}
 }
