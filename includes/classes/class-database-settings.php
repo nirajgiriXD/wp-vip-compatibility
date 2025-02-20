47ded1057beb2a30acb88668d6f1ece8bc120e8f
nirajgirixd <nirajgiri9860@gmail.com>
2025-02-20 23:54:44 +0545

    Show notes and third party's db table source (plugin name)

--- includes/classes/class-database-settings.php
+++ includes/classes/class-database-settings.php
@@ -8,6 +8,7 @@
 namespace WP_VIP_COMPATIBILITY\Includes\Classes;
 
 use WP_VIP_COMPATIBILITY\Includes\Traits\Singleton;
+use WP_VIP_COMPATIBILITY\Includes\Classes\Plugin;
 
 /**
  * This class handles the database submenu settings.
@@ -17,163 +18,163 @@ class Database_Settings {
 	use Singleton;
 
 	/**
-	 * Constructor method is used to initialize the fields.
+	 * Stores plugin data from JSON file.
+	 *
+	 * @var array
 	 */
-	public function __construct() {}
+	private $json_data = [];
 
 	/**
-	 * Render the settings page html.
+	 * Constructor method is used to initialize the fields.
+	 */
+	public function __construct() {
+		$this->json_data = Plugin::get_instance()->get_json_data();
+	}
+
+	/**
+	 * Retrieve the core tables and VIP supported collations from the JSON file.
 	 *
 	 * @return void
 	 */
 	public function render_settings_page() {
 		global $wpdb;
 
-		// Path to the JSON file containing table source data.
-		$table_source_path = WP_VIP_COMPATIBILITY_DIR . '/data/plugins-tables-list.json';
-
-		// Read the JSON file contents
-		$table_source_data = file_get_contents( $table_source_path );
-
-		// Decode the JSON data into an associative array
-		$table_source_data_array = json_decode( $table_source_data, true );
-
-		// Check if decoding was successful
-		if ( null === $table_source_data_array && json_last_error() !== JSON_ERROR_NONE) {
-			$table_source_data_array = array();
-		}
-
-		// Core tables that are not prefixed with 'wp_'.
-		$core_tables = array(
-			'commentmeta'        => array( 'wordpress-core' ),
-			'comments'           => array( 'wordpress-core' ),
-			'links'              => array( 'wordpress-core' ),
-			'options'            => array( 'wordpress-core' ),
-			'postmeta'           => array( 'wordpress-core' ),
-			'posts'              => array( 'wordpress-core' ),
-			'termmeta'           => array( 'wordpress-core' ),
-			'terms'              => array( 'wordpress-core' ),
-			'term_relationships' => array( 'wordpress-core' ),
-			'term_taxonomy'      => array( 'wordpress-core' ),
-			'usermeta'           => array( 'wordpress-core' ),
-			'users'              => array( 'wordpress-core' ),
-		);
-
-		// Merge core tables with the JSON data.
-		$table_source_data_array = array_merge( $table_source_data_array, $core_tables );
-
-		// Supported collations for WordPress VIP.
-		$vip_supported_collations = array(
-			'utf8mb4_general_ci',
-			'utf8mb4_bin',
-			'utf8mb4_unicode_ci',
-			'utf8mb4_icelandic_ci',
-			'utf8mb4_latvian_ci',
-			'utf8mb4_romanian_ci',
-			'utf8mb4_slovenian_ci',
-			'utf8mb4_polish_ci',
-			'utf8mb4_estonian_ci',
-			'utf8mb4_spanish_ci',
-			'utf8mb4_swedish_ci',
-			'utf8mb4_turkish_ci',
-			'utf8mb4_czech_ci',
-			'utf8mb4_danish_ci',
-			'utf8mb4_lithuanian_ci',
-			'utf8mb4_slovak_ci',
-			'utf8mb4_spanish2_ci',
-			'utf8mb4_roman_ci',
-			'utf8mb4_persian_ci',
-			'utf8mb4_esperanto_ci',
-			'utf8mb4_hungarian_ci',
-			'utf8mb4_sinhala_ci',
-			'utf8mb4_german2_ci',
-			'utf8mb4_croatian_mysql561_ci',
-			'utf8mb4_unicode_520_ci',
-			'utf8mb4_vietnamese_ci',
-			'utf8mb4_croatian_ci',
-			'utf8mb4_myanmar_ci',
-			'utf8mb4_thai_520_w2',
-			'utf8mb4_general_nopad_ci',
-			'utf8mb4_nopad_bin',
-			'utf8mb4_unicode_nopad_ci',
-			'utf8mb4_unicode_520_nopad_ci',
-		);
+		// Extract core tables and VIP supported collations from the JSON data.
+		$core_tables              = $this->json_data['core_tables'] ?? [];
+		$vendor_tables            = $this->json_data['vendor_tables'] ?? [];
+		$vip_supported_collations = $this->json_data['vip_supported_collations'] ?? [];
 
 		// Query to get tables with their collation and engine.
-		$tables = $wpdb->get_results("
-			SELECT 
-				TABLE_NAME, 
-				TABLE_COLLATION, 
-				ENGINE 
-			FROM information_schema.TABLES 
-			WHERE TABLE_SCHEMA = '" . DB_NAME . "'"
-		);
+		$tables = $wpdb->get_results("SELECT TABLE_NAME, TABLE_COLLATION, ENGINE FROM information_schema.TABLES WHERE TABLE_SCHEMA = '" . DB_NAME . "'");
 
 		if ( empty( $tables ) ) {
 			echo '<p>' . esc_html__( 'No tables found in the database.', 'wp-vip-compatibility' ) . '</p>';
 			return;
 		}
-		
-		// Start HTML table
+
+		// Start HTML table.
 		echo '<table class="wvc-table">';
 		echo '<thead><tr>
-				<th>' . esc_html__( 'SN','wp-vip-compatibility' ) . '</th>
-				<th>' . esc_html__( 'Table Name','wp-vip-compatibility' ) . '</th>
-				<th>' . esc_html__( 'Engine','wp-vip-compatibility' ) . '</th>
-				<th>' . esc_html__( 'Collation','wp-vip-compatibility' ) . '</th>
-				<th>' . esc_html__( 'Source','wp-vip-compatibility' ) . '</th>
-				<th>' . esc_html__( 'Additional Notes','wp-vip-compatibility' ) . '</th>
+				<th>' . esc_html__( 'SN', 'wp-vip-compatibility' ) . '</th>
+				<th>' . esc_html__( 'Table Name', 'wp-vip-compatibility' ) . '</th>
+				<th>' . esc_html__( 'Engine', 'wp-vip-compatibility' ) . '</th>
+				<th>' . esc_html__( 'Collation', 'wp-vip-compatibility' ) . '</th>
+				<th>' . esc_html__( 'Source', 'wp-vip-compatibility' ) . '</th>
+				<th>' . esc_html__( 'VIP Compatibility', 'wp-vip-compatibility' ) . '</th>
+				<th>' . esc_html__( 'Notes', 'wp-vip-compatibility' ) . '</th>
 			  </tr></thead>';
 		echo '<tbody>';
-	
-		// Loop through the tables and check for compatibility
+
 		$counter = 1;
 		foreach ( $tables as $table ) {
-			$engine                = $table->ENGINE;
-			$vip_supported         = in_array( $table->TABLE_COLLATION, $vip_supported_collations );
-			$has_supported_prefix  = strpos( $table->TABLE_NAME, 'wp_' ) === 0;
-			$additional_note_class = 'not-compatible';
-			$table_name            = $table->TABLE_NAME;
+			$table_name           = $table->TABLE_NAME;
+			$engine               = $table->ENGINE;
+			$collation            = $table->TABLE_COLLATION;
+			$vip_supported        = in_array( $collation, $vip_supported_collations );
+			$has_supported_prefix = strpos( $table_name, 'wp_' ) === 0;
 
-			// First, check if the table is in the JSON file as it is.
-			$source = isset( $table_source_data_array[ $table_name ] ) ? implode( ', ', $table_source_data_array[ $table_name ] ) : null;
+			// Determine table source.
+			$source = $this->get_table_source( $table_name, $core_tables, $vendor_tables, $has_supported_prefix );
 
-			// If not found, try removing the 'wp_' prefix and check again.
-			if ( ! $source && $has_supported_prefix ) {
-				$prefix_removed_table_name = preg_replace( '/^wp_/', '', $table_name );
-				$source = isset( $table_source_data_array[ $prefix_removed_table_name ] ) ? implode( ', ', $table_source_data_array[ $prefix_removed_table_name ] ) : '-';
-			} else {
-				$source = $source ?: '-';
-			}
+			// Determine compatibility.
+			$compatibility_class = 'compatible';
+			$compatibility       = esc_html__( 'Compatible', 'wp-vip-compatibility' );
+			$notes               = [];
 
-			// Determine additional notes.
-			$notes = array();
+			// Check collation compatibility.
 			if ( ! $vip_supported ) {
-				$notes[] = esc_html__( 'Unsupported Collation', 'wp-vip-compatibility' );
-			} 
-			if ( $engine != 'InnoDB' ) {
-				$notes[] = 'Incompatible Engine';
+				$suggested_collation = $this->get_suggested_collation( $collation, $vip_supported_collations );
+				$notes[] = esc_html__( 'The collation is unsupported, consider using', 'wp-vip-compatibility' ) . 
+					' <code>' . esc_html( $suggested_collation ) . '</code> ' . esc_html__( 'for VIP compatibility', 'wp-vip-compatibility' );
+				$compatibility_class = 'not-compatible';
+				$compatibility       = esc_html__( 'Not Compatible', 'wp-vip-compatibility' );
 			}
+
+			// Check engine compatibility.
+			if ( $engine !== 'InnoDB' ) {
+				$notes[] = esc_html__( 'The engine is unsupported, consider using', 'wp-vip-compatibility' ) . 
+					' <code>InnoDB</code> ' . esc_html__( 'for VIP compatibility', 'wp-vip-compatibility' );
+				$compatibility_class = 'not-compatible';
+				$compatibility       = esc_html__( 'Not Compatible', 'wp-vip-compatibility' );
+			}
+
+			// Check prefix compatibility.
 			if ( ! $has_supported_prefix ) {
-				$notes[] = esc_html__( 'Unsupported Prefix', 'wp-vip-compatibility' );
+				$notes[] = esc_html__( 'The table prefix is unsupported, consider using', 'wp-vip-compatibility' ) . 
+					' <code>wp_</code> ' . esc_html__( 'prefix for VIP compatibility', 'wp-vip-compatibility' );
+				$compatibility_class = 'not-compatible';
+				$compatibility       = esc_html__( 'Not Compatible', 'wp-vip-compatibility' );
 			}
-			if ( empty( $notes ) ) {
-				$notes[] = esc_html__( 'Compatible', 'wp-vip-compatibility' );
-				$additional_note_class = 'compatible';
-			}
-	
-			// Echo row
+
+			// If no issues, set the notes to '-'.
+			$notes = empty( $notes ) ? ['-'] : $notes;
+
+			// Output table row.
 			echo '<tr>';
 			echo '<td>' . esc_html( $counter++ ) . '</td>';
 			echo '<td>' . esc_html( $table_name ) . '</td>';
-			echo '<td>' . esc_html( $table->ENGINE ) . '</td>';
-			echo '<td>' . esc_html( $table->TABLE_COLLATION ) . '</td>';
+			echo '<td>' . esc_html( $engine ) . '</td>';
+			echo '<td>' . esc_html( $collation ) . '</td>';
 			echo '<td>' . esc_html( $source ) . '</td>';
-			echo '<td class="' . $additional_note_class . '">' . esc_html( implode( ', ', $notes ) ) . '</td>';
+			echo '<td class="' . esc_attr( $compatibility_class ) . '">' . esc_html( $compatibility ) . '</td>';
+			echo '<td><ul><li>' . implode( '</li><li>', $notes ) . '</li></ul></td>';
 			echo '</tr>';
 		}
-	
+
 		echo '</tbody></table>';
 	}
 
+	/**
+	 * Get table source from core tables or vendor tables.
+	 *
+	 * @param string $table_name          The database table name.
+	 * @param array  $core_tables         List of core tables.
+	 * @param array  $vendor_tables       List of vendor tables.
+	 * @param bool   $has_supported_prefix Whether the table has a supported prefix.
+	 *
+	 * @return string
+	 */
+	private function get_table_source( $table_name, $core_tables, $vendor_tables, $has_supported_prefix ) {
+		// Check core tables.
+		if ( isset( $core_tables[ $table_name ] ) ) {
+			return implode( ', ', $core_tables[ $table_name ] );
+		}
+
+		// Check vendor tables.
+		if ( isset( $vendor_tables[ $table_name ] ) ) {
+			return implode( ', ', $vendor_tables[ $table_name ] );
+		}
+
+		// If the table starts with "wp_", try checking without the prefix.
+		if ( $has_supported_prefix ) {
+			$trimmed_table_name = preg_replace( '/^wp_/', '', $table_name );
+
+			if ( isset( $core_tables[ $trimmed_table_name ] ) ) {
+				return implode( ', ', $core_tables[ $trimmed_table_name ] );
+			}
+
+			if ( isset( $vendor_tables[ $trimmed_table_name ] ) ) {
+				return implode( ', ', $vendor_tables[ $trimmed_table_name ] );
+			}
+		}
+
+		return '-';
+	}
+
+	/**
+	 * Get the closest VIP-supported collation suggestion.
+	 *
+	 * @param string $current_collation   The table's current collation.
+	 * @param array  $vip_collations      List of VIP-supported collations.
+	 *
+	 * @return string
+	 */
+	private function get_suggested_collation( $current_collation, $vip_collations ) {
+		foreach ( $vip_collations as $vip_collation ) {
+			if ( strpos( $vip_collation, explode('_', $current_collation)[0] ) === 0 ) {
+				return $vip_collation;
+			}
+		}
+		return 'Not Supported';
+	}
 }
