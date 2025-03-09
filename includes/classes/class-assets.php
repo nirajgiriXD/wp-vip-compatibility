d0188aa7bd581c8018d795d2226fe76f6ce337e5
nirajgirixd <nirajgiri9860@gmail.com>
2025-03-09 17:59:21 +0545

    Enqueue chart.js

--- includes/classes/class-assets.php
+++ includes/classes/class-assets.php
@@ -41,6 +41,7 @@ class Assets {
 	 */
 	public function admin_enqueue_scripts() {
 
+		// Enqueue custom script.
 		if ( file_exists( WP_VIP_COMPATIBILITY_DIR . '/assets/js/admin.js' ) ) {
 			wp_register_script(
 				'wp-vip-compatibility-admin-script',
@@ -57,15 +58,30 @@ class Assets {
 				[
 					'ajax_url' => admin_url( 'admin-ajax.php' ),
 					'nonce'    => wp_create_nonce( 'wvc_ajax_nonce' ),
-					'i18n'     => [
+					'i18n'     => array(
 						'checking'                => esc_html__( 'Checking...', 'wp-vip-compatibility' ),
 						'error'                   => esc_html__( 'Error', 'wp-vip-compatibility' ),
+						'unableToFetchData'       => esc_html__( 'Unable to fetch data.', 'wp-vip-compatibility' ),
 						'unableToFetchLogDetails' => esc_html__( 'Unable to fetch log details.', 'wp-vip-compatibility' ),
-					],
+						'noDataAvailable'         => esc_html__( 'No data available.', 'wp-vip-compatibility' ),
+						'noChartData'             => esc_html__( 'No chart data available.', 'wp-vip-compatibility' ),
+					),
 				]
 			);
 		}
 
+		// Enqueue chart.js.
+		if ( file_exists( WP_VIP_COMPATIBILITY_DIR . '/assets/js/chart.js' ) ) {
+			wp_enqueue_script(
+				'wp-vip-compatibility-chart-js',
+				WP_VIP_COMPATIBILITY_URL . '/assets/js/chart.js',
+				array(),
+				filemtime( WP_VIP_COMPATIBILITY_DIR . '/assets/js/chart.js' )
+			);
+			wp_enqueue_script( 'wp-vip-compatibility-chart-js' );
+		}
+
+		// Enqueue custom style.
 		if ( file_exists( WP_VIP_COMPATIBILITY_DIR . '/assets/css/admin.css' ) ) {
 			wp_register_style(
 				'wp-vip-compatibility-admin-styles',
